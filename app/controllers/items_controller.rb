2251cb24957474e8914f444ecdea581595461c63
ma-sensei-web <kimu120558@gmail.com>
2021-05-10 21:59:51 +0900

    商品出品ページを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+
   def index
+    @items = Item.includes(:user).order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
   end
 
   private
-  def prototype_params
+  def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :status_id, :charge_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
