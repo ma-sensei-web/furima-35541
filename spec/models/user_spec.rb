a3cface94212678122978bab580facd5bfac6577
ma-sensei-web <kimu120558@gmail.com>
2021-05-10 12:19:31 +0900

    新規登録/ログイン/ログアウトのテストコード修正3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -134,6 +134,12 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('First name reading Full-width katakana characters')
       end
+
+      it 'birth_dateが空だと保存できない' do
+        @user.birth_date = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birth date can't be blank")
+      end
     end
   end
 end
