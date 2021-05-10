3311fb370d08d16034c0b9df5d8f996551474f01
ma-sensei-web <kimu120558@gmail.com>
2021-05-10 17:56:16 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :status, :charge, :area, :day
+
   belongs_to :user
   has_one_attached :image 
 end
