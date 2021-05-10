0bcd90081df8c81c68cc2cd14e0b10442432c690
ma-sensei-web <kimu120558@gmail.com>
2021-05-10 18:36:54 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,27 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :status, :charge, :area, :day
-
+  belongs_to :category
+  belongs_to :status
+  belongs_to :charge
+  belongs_to :area
+  belongs_to :day
   belongs_to :user
-  has_one_attached :image 
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explanation
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :charge_id
+    validates :area_id
+    validates :day_id
+  end
+
+  validates :price, numericality: true
 end
