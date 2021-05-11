2722c847cf412d5840c43300e0824f7f995a09f9
ma-sensei-web <kimu120558@gmail.com>
2021-05-11 12:30:59 +0900

    モデルの単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,29 +1,28 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
   belongs_to :charge
   belongs_to :area
   belongs_to :day
-  belongs_to :user
-  has_one_attached :image
-
+  
   with_options presence: true do
     validates :image
     validates :name
     validates :explanation
-    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
   end
-
-  with_options numericality: { other_than: 1 } do
+  
+  validates :price, presence: true, numericality: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is out of setting range" }
+  
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
     validates :category_id
     validates :status_id
     validates :charge_id
+    validates :area_id
     validates :day_id
   end
-
-  validates :area_id, numericality: { other_than: 0 }
-
-  validates :price, numericality: true
-  # , message: 'is invalid. Input harf-widh characters'
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+    name        {Faker::Lorem.sentence}
+    explanation {Faker::Lorem.sentence}
+    category_id {2}
+    status_id   {2}
+    charge_id   {2}
+    area_id     {2}
+    day_id      {2}
+    price       {2000}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/item-sample.png'), filename: 'item-sample.png')
+    end
   end
 end
