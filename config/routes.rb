ece78d19b73450169e532142e7a49d7ac2c7f5ec
ma-sensei-web <kimu120558@gmail.com>
2021-05-24 11:03:55 +0900

    パンくずリストを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
+  get 'cards/new'
+  get 'users/show'
   devise_for :users
   root to: 'items#index'
   resources :items do
-    resources :orders, only: [:index, :create]
+    resources :orders, only: [:index, :create]    
   end
+  
+  get 'items/index'
+  get 'orders/index'
+
+  # resources :users, only: [:show, :update]
+  resources :users, only: [:show]
+  resources :cards, only: [:new, :create]
+  resources :items, only: :order do
+    post 'order', on: :member
+  end
+
+
 end
